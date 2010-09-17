36a4eced5a8ca44c50e9bead2cabac342c71f77a
Colin Samuel Rosenthal <csr@statsbiblioteket.dk>
2010-09-17 13:24:26 +0000

    Added parameter initialisation.

--- modules/radiotv/test/dk/statsbiblioteket/doms/radiotv/extractor/BroadcastExtractionServiceTest.java
+++ modules/radiotv/test/dk/statsbiblioteket/doms/radiotv/extractor/BroadcastExtractionServiceTest.java
@@ -12,60 +12,35 @@ import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 
 import junit.framework.TestCase;
 
-public class BroadcastExtractionServiceTest extends TestCase {
-
-	private final String baseUri = "http://localhost:9998/";
-	private SelectorThread threadSelector; 
+public class BroadcastExtractionServiceTest extends GrizzlyTestCase {
 
 	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		final Map<String, String> initParams = new HashMap<String, String>();
-		initParams.put("com.sun.jersey.config.property.packages", "dk.statsbiblioteket.doms.radiotv.extractor");
-		System.out.println("Starting grizzly...");
-		threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
-		System.out.println("Grizzly started.");
-	}
+    protected void setUp() throws Exception {
+            super.setUp();
+    }
 
-	public void testDummyInterfaceProgramClipStatus() throws Exception {
-		Client c = Client.create();
-        WebResource r = c.resource(baseUri).path("/bes/statusofprogramclip").queryParam("channel", "DR");
-        String responseMsg = r.get(String.class);
-        System.out.println("ResponseMsg: " + responseMsg);
-		assertNotNull(responseMsg);
-        assertNotSame("", responseMsg);
-        assertTrue(responseMsg.contains("DR"));
-	}
 
-	public void testDummyInterfaceCreateProgramClip() throws Exception {
-		Client c = Client.create();
-        WebResource r = c.resource(baseUri);
-        String responseMsg = r.path("/bes/createprogramclip").get(String.class);
-		assertEquals("Request for generating program clip.", responseMsg);
-	}
-
-	public void testDummyInterfaceGetProgramClip() throws Exception {
-		Client c = Client.create();
-        WebResource r = c.resource(baseUri);
-        String responseMsg = r.path("/bes/getprogramclip").get(String.class);
-		assertEquals("Dette skal v�re en str�m af video...", responseMsg);
-	}
 
     public void testGetObjectStatus() throws Exception {
         ClientConfig cc =new DefaultClientConfig();
-        cc.getClasses().add(JsonBodyWriter.class);
         Client c = Client.create(cc);
         WebResource r = c.resource(baseUri).path("/bes/getobjectstatus").queryParam("programpid", "12345");
         ObjectStatus status = r.get(ObjectStatus.class);
-        assertTrue(status.getUrl().contains("URL"));
+        assertTrue(status.getStatus().equals(ObjectStatusEnum.STARTING));
     }
 
+    public void testParameterSet() {
+         ClientConfig cc =new DefaultClientConfig();
+        Client c = Client.create(cc);
+        WebResource r = c.resource(baseUri).path("/bes");
+        String output = r.get(String.class);
+        assertTrue(Transcoder.tempdir.equals("tempdir"));
+        assertTrue(Transcoder.finaldir.equals("clips"));
+    }
 
 	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
-		System.out.println("Stopping grizzly...");
-		threadSelector.stopEndpoint();
-		System.out.println("Grizzly stopped.");
+
 	}
 }
