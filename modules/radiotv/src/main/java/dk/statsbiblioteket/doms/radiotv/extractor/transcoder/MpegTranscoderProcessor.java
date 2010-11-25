3045073469c96c681f4545076e9dad5ff774b0e7
Colin Samuel Rosenthal <csr@statsbiblioteket.dk>
2010-11-25 09:11:42 +0000

    BART multiclipping

--- modules/radiotv/src/main/java/dk/statsbiblioteket/doms/radiotv/extractor/transcoder/MpegTranscoderProcessor.java
+++ modules/radiotv/src/main/java/dk/statsbiblioteket/doms/radiotv/extractor/transcoder/MpegTranscoderProcessor.java
@@ -22,6 +22,8 @@
 package dk.statsbiblioteket.doms.radiotv.extractor.transcoder;
 
 import javax.servlet.ServletConfig;
+import java.util.List;
+import java.io.File;
 
 public class MpegTranscoderProcessor extends ProcessorChainElement {
 
@@ -34,14 +36,46 @@ public class MpegTranscoderProcessor extends ProcessorChainElement {
     private void mpegClip(TranscodeRequest request, ServletConfig config) throws ProcessorException {
         long blocksize = 1880L;
         final int clipSize = request.getClips().size();
-        if (clipSize > 1) throw new ProcessorException("Haven't implemented mpeg mulitclipping yet");
-        TranscodeRequest.FileClip clip = request.getClips().get(0);
-        String start = "";
-        if (clip.getStartOffsetBytes() != null && clip.getStartOffsetBytes() != 0L) start = "skip=" + clip.getStartOffsetBytes()/blocksize;
-        String length = "";
-        if (clip.getClipLength() != null && clip.getClipLength() != 0L) length = "count=" + clip.getClipLength()/blocksize;
-        String command = "dd if=" + clip.getFilepath() + " bs=" + blocksize + " " + start + " " + length + "| "
-                + FlashTranscoderProcessor.getFfmpegCommandLine(request, config);
+        String command;
+        if (clipSize > 1) {
+            command = getMultiClipCommand(request, config);
+        } else {
+            TranscodeRequest.FileClip clip = request.getClips().get(0);
+            String start = "";
+            if (clip.getStartOffsetBytes() != null && clip.getStartOffsetBytes() != 0L) start = "skip=" + clip.getStartOffsetBytes()/blocksize;
+            String length = "";
+            if (clip.getClipLength() != null && clip.getClipLength() != 0L) length = "count=" + clip.getClipLength()/blocksize;
+            command = "dd if=" + clip.getFilepath() + " bs=" + blocksize + " " + start + " " + length + "| "
+                    + FlashTranscoderProcessor.getFfmpegCommandLine(request, config);
+        }
         FlashTranscoderProcessor.runClipperCommand(command);
     }
+
+    private String getMultiClipCommand(TranscodeRequest request, ServletConfig config) {
+        String files = "";
+        long start = 0L;
+        long length = 0L;
+        long blocksize = 1880L;
+        List<TranscodeRequest.FileClip> clips = request.getClips();
+        for (int i=0; i<clips.size(); i++) {
+            TranscodeRequest.FileClip clip = clips.get(i);
+            files += " " + clip.getFilepath() + " ";
+            if (clip.getClipLength() != null) {
+                length += clip.getClipLength()/blocksize;
+            } else if (clip.getStartOffsetBytes() != null) {
+                length += ((new File(clip.getFilepath())).length() - clip.getStartOffsetBytes())/blocksize;
+            } else {
+                length += (new File(clip.getFilepath())).length()/blocksize;
+            }
+            if (i == 0 && clip.getStartOffsetBytes() != null) {
+                start = clip.getStartOffsetBytes()/blocksize;
+            }
+        }
+        String command = "cat " + files
+                + " |dd bs=" + blocksize + " skip=" + start + " count=" + length + "| "
+                + FlashTranscoderProcessor.getFfmpegCommandLine(request, config);
+        return command;
+    }
+
+
 }
--- modules/radiotv/src/main/java/dk/statsbiblioteket/doms/radiotv/extractor/transcoder/WavTranscoderProcessor.java
+++ modules/radiotv/src/main/java/dk/statsbiblioteket/doms/radiotv/extractor/transcoder/WavTranscoderProcessor.java
@@ -24,25 +24,57 @@ package dk.statsbiblioteket.doms.radiotv.extractor.transcoder;
 import dk.statsbiblioteket.doms.radiotv.extractor.Constants;
 
 import javax.servlet.ServletConfig;
+import java.util.List;
+import java.io.File;
 
 public class WavTranscoderProcessor extends ProcessorChainElement {
     @Override
     protected void processThis(TranscodeRequest request, ServletConfig config) throws ProcessorException {
         long blocksize = 1880L;
         final int clipSize = request.getClips().size();
-        if (clipSize > 1) throw new ProcessorException("Haven't implemented wav mulitclipping yet");
-        TranscodeRequest.FileClip clip = request.getClips().get(0);
-        String start = "";
-        if (clip.getStartOffsetBytes() != null && clip.getStartOffsetBytes() != 0L) start = "skip=" + clip.getStartOffsetBytes()/blocksize;
-        String length = "";
-        if (clip.getClipLength() != null && clip.getClipLength() != 0L) length = "count=" + clip.getClipLength()/blocksize;
-        String command = "dd if=" + clip.getFilepath() + " bs=" + blocksize + " " + start + " " + length + "| "
-                + getFfmpegMp3CommandLine(request, config);
+        String command;
+        if (clipSize > 1) {
+            command = getMultiClipCommand(request, config);
+        } else {
+            TranscodeRequest.FileClip clip = request.getClips().get(0);
+            String start = "";
+            if (clip.getStartOffsetBytes() != null && clip.getStartOffsetBytes() != 0L) start = "skip=" + clip.getStartOffsetBytes()/blocksize;
+            String length = "";
+            if (clip.getClipLength() != null && clip.getClipLength() != 0L) length = "count=" + clip.getClipLength()/blocksize;
+            command = "dd if=" + clip.getFilepath() + " bs=" + blocksize + " " + start + " " + length + "| "
+                    + getFfmpegMp3CommandLine(request, config);
+        }
         FlashTranscoderProcessor.runClipperCommand(command);
     }
 
     private static String getFfmpegMp3CommandLine(TranscodeRequest request, ServletConfig config) {
          return "ffmpeg -i - -ab " + config.getInitParameter(Constants.AUDIO_BITRATE) + "000 " + Util.getMp3File(request, config);
+    }
+
+    private String getMultiClipCommand(TranscodeRequest request, ServletConfig config) {
+        String files = "";
+        long start = 0L;
+        long length = 0L;
+        long blocksize = 1880L;
+        List<TranscodeRequest.FileClip> clips = request.getClips();
+        for (int i=0; i<clips.size(); i++) {
+            TranscodeRequest.FileClip clip = clips.get(i);
+            files += " " + clip.getFilepath() + " ";
+            if (clip.getClipLength() != null) {
+                length += clip.getClipLength()/blocksize;
+            } else if (clip.getStartOffsetBytes() != null) {
+                length += ((new File(clip.getFilepath())).length() - clip.getStartOffsetBytes())/blocksize;
+            } else {
+                length += (new File(clip.getFilepath())).length()/blocksize;
+            }
+            if (i == 0 && clip.getStartOffsetBytes() != null) {
+                start = clip.getStartOffsetBytes()/blocksize;
+            }
+        }
+        String command = "cat " + files
+                + " |dd bs=" + blocksize + " skip=" + start + " count=" + length + "| "
+                + getFfmpegMp3CommandLine(request, config);
+        return command;
     }
 
 }
