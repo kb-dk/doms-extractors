9592c9f88f610de3914789d76ca3da69eba50312
Henning Böttger <heb@statsbiblioteket.dk>
2011-11-24 13:56:32 +0000

    Added roundrobin batch execution of mass transcoding

--- modules/radiotv/etc/input_data/mass_transcode.sh
+++ modules/radiotv/etc/input_data/mass_transcode.sh
@@ -3,8 +3,11 @@
 DOMS=http://alhena:7880/fedora
 USER=fedoraReadOnlyAdmin
 PASS=fedoraReadOnlyPass
-BES=http://iapetus:9311/bes_DEVEL_EXTRACTION
-#BES=http://iapetus:9311/bes_DEVEL_OFFLINE
+
+BES_SERVER_1=http://iapetus:9311/bes_DEVEL_EXTRACTION_1
+BES_SERVER_2=http://iapetus:9311/bes_DEVEL_EXTRACTION_2
+BES_SERVER_3=http://iapetus:9311/bes_DEVEL_EXTRACTION_3
+BES_SERVER_4=http://iapetus:9311/bes_DEVEL_EXTRACTION_4
 
 # Transcode programs
 METHOD="getobjectstatus"
@@ -13,10 +16,21 @@ METHOD="getobjectstatus"
 # Transcode snapshots
 #METHOD="getsnapshotstatus"
 
+NEXT_BES_SERVER="${BES_SERVER_1}"
 while read domspid
 do
-    echo "Queuing transcoding for $domspid"
-    wget -O - "${BES}/rest/bes/${METHOD}?programpid="$domspid
+	BES="${NEXT_BES_SERVER}"
+	if [ ${NEXT_BES_SERVER} == ${BES_SERVER_1} ]; then
+		NEXT_BES_SERVER="${BES_SERVER_2}"
+	elif [ ${NEXT_BES_SERVER} == ${BES_SERVER_2} ]; then
+		NEXT_BES_SERVER="${BES_SERVER_3}"
+	elif [ ${NEXT_BES_SERVER} == ${BES_SERVER_3} ]; then
+		NEXT_BES_SERVER="${BES_SERVER_4}"
+	else
+		NEXT_BES_SERVER="${BES_SERVER_1}"
+	fi 
+    echo "Queuing transcoding for $domspid on server ${BES}"
+    #wget -O - "${BES}/rest/bes/${METHOD}?programpid="$domspid
     sleep 0.1s
 done
 
