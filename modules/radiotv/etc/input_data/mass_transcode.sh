089890a7d5ade7c062754e21abd52bb76926ecd1
Henning Böttger <heb@statsbiblioteket.dk>
2011-11-24 12:55:22 +0000

    Updated scripts after test of release candidate

--- modules/radiotv/etc/input_data/mass_transcode.sh
+++ modules/radiotv/etc/input_data/mass_transcode.sh
@@ -4,12 +4,19 @@ DOMS=http://alhena:7880/fedora
 USER=fedoraReadOnlyAdmin
 PASS=fedoraReadOnlyPass
 BES=http://iapetus:9311/bes_DEVEL_EXTRACTION
+#BES=http://iapetus:9311/bes_DEVEL_OFFLINE
 
+# Transcode programs
+METHOD="getobjectstatus"
+# Transcode previews
+#METHOD="getpreviewstatus"
+# Transcode snapshots
+#METHOD="getsnapshotstatus"
 
 while read domspid
 do
     echo "Queuing transcoding for $domspid"
-    wget -O - $BES"/rest/bes/getobjectstatus?programpid="$domspid
+    wget -O - "${BES}/rest/bes/${METHOD}?programpid="$domspid
     sleep 0.1s
 done
 
--- modules/radiotv/etc/input_data/query_doms_for_shard_pids_with_wav.sh
+++ modules/radiotv/etc/input_data/query_doms_for_shard_pids_with_wav.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo Remember to update DOMS server name and port in the script
+# Remember to update DOMS server name and port in the script
 
 ALL_PID_TO_FILE_MAP="all_pid-file_map.txt"
 SHARD_PID_FOR_WAVE_FILES="shard_pid_for_wave_files.txt"
 
 # Outputs all shard pids from DOMS
-curl -o "$ALL_PID_TO_FILE_MAP" -u fedoraReadOnlyAdmin:fedoraReadOnlyPass 'http://server:port/fedora/risearch?type=tuples&lang=iTQL&flush=true&format=CSV&query=select%20%24label%20%20%24url%20from%20%3C%23ri%3E%20where%20%24object%20%3Cfedora-model%3Alabel%3E%20%24label%20and%20%24object%20%3Cfedora-model%3AhasModel%3E%20%3Cfedora%3Adoms%3AContentModel_Shard%3E%20and%20%24object%20%3Cfedora-model%3Astate%3E%20%3Cfedora-model%3AActive%3E%20and%20%24object%20%3Chttp%3A%2F%2Fdoms.statsbiblioteket.dk%2Frelations%2Fdefault%2F0%2F1%2F%23consistsOf%3E%20%24file%20and%20%24file%20%3Cfedora-model%3Alabel%3E%20%24url'
+curl -o "$ALL_PID_TO_FILE_MAP" -u fedoraReadOnlyAdmin:fedoraReadOnlyPass 'http://server:7880/fedora/risearch?type=tuples&lang=iTQL&flush=true&format=CSV&query=select%20%24label%20%20%24url%20from%20%3C%23ri%3E%20where%20%24object%20%3Cfedora-model%3Alabel%3E%20%24label%20and%20%24object%20%3Cfedora-model%3AhasModel%3E%20%3Cfedora%3Adoms%3AContentModel_Shard%3E%20and%20%24object%20%3Cfedora-model%3Astate%3E%20%3Cfedora-model%3AActive%3E%20and%20%24object%20%3Chttp%3A%2F%2Fdoms.statsbiblioteket.dk%2Frelations%2Fdefault%2F0%2F1%2F%23consistsOf%3E%20%24file%20and%20%24file%20%3Cfedora-model%3Alabel%3E%20%24url'
 
 # Filter wav files and extract pid
-grep "\.wav" "$ALL_PID_TO_FILE_MAP" | grep -o "uuid:[^,]*" | sed 's/uuid://'
+grep "\.wav" "$ALL_PID_TO_FILE_MAP" | grep -o "uuid:[^,]*"
