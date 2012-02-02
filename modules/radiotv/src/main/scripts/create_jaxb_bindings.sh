b6d2c8e6faae1154a2e0130006ec64bdee54c5cc
Colin Samuel Rosenthal <csr@statsbiblioteket.dk>
2012-02-02 10:16:41 +0000

    Build scripts for release.

---
+++ modules/radiotv/src/main/scripts/create_jaxb_bindings.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xjc ../resources/context.xsd -p dk.statsbiblioteket.doms.radiotv.extractor.servlet -d ../java
--- modules/radiotv/src/main/scripts/updater.sh
+++ modules/radiotv/src/main/scripts/updater.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CLASSPATH=${HOME}/services/webapps/bes_CSR/WEB-INF/lib/BroadcastExtractionService*.jar
-java -cp ${CLASSPATH} -Ddk.statsbiblioteket.radiotv.extractor.updateidentifier.configdir=${HOME}/bes/config -Dlog4j.configuration=file://${HOME}/bes/config/log4j.updater.xml dk.statsbiblioteket.doms.radiotv.extractor.updateidentifier.UpdateIdentifierApplication
+CLASSPATH=../lib/BroadcastExtractionService*.jar
+java -cp ${CLASSPATH} -Ddk.statsbiblioteket.radiotv.extractor.updateidentifier.configdir=../conf -Dlog4j.configuration=file://../conf/log4j.updater.xml dk.statsbiblioteket.doms.radiotv.extractor.updateidentifier.UpdateIdentifierApplication
 
